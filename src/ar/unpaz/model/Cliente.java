1bcbf541bce4092dab44beb33dd10ba5cd96b6f9
Javier Molina <elmacdev@gmail.com>
2024-04-15 19:48:59 -0300

    un nuevo comentario

--- src/ar/unpaz/model/Cliente.java
+++ src/ar/unpaz/model/Cliente.java
@@ -4,6 +4,28 @@ public class Cliente {
 	
 	public String nombre;
 	public String apellido;
+	public String direccion;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	
 	
 	
 
